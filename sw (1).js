98e5dad3d52a3e1c588bb9a36285e3c7ea65c90e
sanjaya1985-lab <sanjaya1985.ai@gmail.com>
2026-07-07 10:49:54 +0530

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'rental-calc-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch cached resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
